43259e4959cecf6772750792a59a93b6e54a141e
Coronel-GD <g.coronel2005@gmail.com>
2025-11-07 19:15:15 -0300

    probando la funcion

--- funciones/PotenciaCoronel.py
+++ funciones/PotenciaCoronel.py
@@ -1,4 +1,4 @@
-def PotenciaCoronel(base, exponente):
+def PotenciaCoronel(base, exponente):     #probando cambios en el test Coronel
     """
     Devuelve la base elevada al exponente.
     Ejemplo: Potencia_coronel(2, 3) → 8
