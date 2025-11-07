0ddba0454f57873b45bfe18898a076b1ee3ab2ec
lucas royano <royanolucas@.com>
2025-11-07 16:30:39 -0300

    mensaje

--- tests/test_factorial_royano.py
+++ tests/test_factorial_royano.py
@@ -1,5 +1,5 @@
 #tests/test_factorial.py
-from funciones.factorial import factorial
+from funciones.factorial_royano import factorial
 def test_factorial():
  assert factorial(5) == 120
  assert factorial(-3) is None
